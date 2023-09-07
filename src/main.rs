efedfa21e2895f67e384fc3afadf7531376ebf16
Caleb <calebtheeperson@gmail.com>
2023-09-07 14:12:41 -0700

    Added Player & Room Struct, with some stubs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+enum School {
+    HarveyMudd,
+    Pomona,
+    Pitzer,
+    Scripps,
+    Cmc
 }
+
+struct Player {
+    name: String,
+    hunger: u8, // Biggest Number in a u8 is 255
+    time: u8, // Maybe make this a float , look at later
+    health: u8,
+    item: String,
+}
+
+impl Player {
+    pub fn test(&mut self){
+        println!("{}",self.hunger)
+    }
+}
+
+
+struct Room {
+    school: School,
+    megaEvent: Option<Event>,
+    events: Option<Vec<Event>> // Max 1-3
+}
+
+
+struct Event {
+
+}
+
+
+
+fn main() {
+    let mut Cable = Player{name: "Caleb".into(), hunger: 100, time: 100, health:100, item: "Skateboard".into()};
+    let testRoom: Room = Room{school: School::HarveyMudd, megaEvent: None, events: None};
+}
+

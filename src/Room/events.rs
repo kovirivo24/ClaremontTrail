8ce954b8e6bf51a96a41a2bd9aa872ad4e71ec10
kovirivo24 <kovit.virivong@gmail.com>
2023-09-11 16:29:15 -0700

    impl addEvents func

--- src/Room/events.rs
+++ src/Room/events.rs
@@ -2,22 +2,22 @@ pub mod events {
     pub struct events {
         pub name: String,
         message: String,
-        mega_events: bool,
-        events: Option<Vec<events>>, // eventss can lead to other eventss,
+        megaEvent: bool,
+        events: Option<Vec<events>>,
         chance: u8,                  // make this a float, or choose arbitary max
         time: u8,
         health: u8,
         hunger: u8,
     }
-
+    
     // implements default values for each events -> let p = events { name: "Lunch", ..Default::default() };
     impl Default for events {
         fn default() -> events {
             events {
                 name: "Untitledevents".into(),
                 message: "EmptyMessage".into(),
-                mega_events: false,
-                events: None, //set to walk?
+                megaEvent: false,
+                events: None,
                 chance: 1,    // lets set a default attributes
                 time: 5,
                 health: 0,
@@ -30,19 +30,29 @@ pub mod events {
         pub fn getName(self) -> String {
             (self.name)
         }
-
+    
         pub fn getMessage(self) -> String {
             (self.message)
         }
-
+    
         pub fn getMegaEvent(self) {}
-
+    
         pub fn getEvents(self) {}
-
+    
+        pub fn addEvents(&mut self, mut event: Vec<events>) {
+            let mut curEvent = self.events;
+            if curEvent.is_none() {
+                curEvent = Some(event)
+            } else {
+                curEvent.unwrap().append(&mut event)
+            }
+            self.events = curEvent
+        }
+    
         pub fn getChance(self) -> u8 {
             (self.chance)
         }
-
+    
         pub fn getHealth(self) -> u8 {
             (self.health)
         }
@@ -50,7 +60,7 @@ pub mod events {
             (self.hunger)
         }
     }
-
+    
     // going to have to restructure a lot of this
     pub fn createevents() -> Vec<events> {
         // let events1 = {
@@ -73,15 +83,84 @@ pub mod events {
         //     }
         // };
         // let events = vec![events1, events2];
-
+    
         //For some reason the above code had an issue with creating two variables then putting it into a vector ??, so i  just took one and put it inside the vector for testing purposses.
-        let eventsVec = vec![events {
-            name: "Running".into(),
-            message: "You run.".into(),
-            mega_events: false,
-            ..Default::default()
-        }];
-
+        let mut eventsVec = vec![
+            events {
+                name: "Running".into(),
+                message: "You run.".into(),
+                megaEvent: true,
+                ..Default::default()
+            },
+            events {
+                name: "Skateboarding".into(),
+                message: "You skateboard.".into(),
+                megaEvent: true,
+                ..Default::default()
+            },
+            events {
+                name: "RunIntoStairs".into(),
+                message: "You just ran into a set of stairs. Ouch".into(),
+                megaEvent: false,
+                health: 2,
+                ..Default::default()
+            },
+            events {
+                name: "RunIntoFootballPlayers".into(),
+                message: "You just ran into football players. Ouch".into(),
+                megaEvent: false,
+                health: 5,
+                ..Default::default()
+            },
+            events {
+                name: "RunIntoCar".into(),
+                message: "You just ran into a car. Watch out".into(),
+                megaEvent: false,
+                health: 8,
+                ..Default::default()
+            },
+            events {
+                name: "RunIntoCactus".into(),
+                message: "You just ran into a cactus. Be careful".into(),
+                megaEvent: false,
+                health: 7,
+                ..Default::default()
+            },
+            events {
+                name: "LunchAtFrary".into(),
+                message: "You are near Frary Dining Hall. Go in for lunch?".into(),
+                megaEvent: true,
+                ..Default::default()
+            },
+            events {
+                name: "CrowdedLunch".into(),
+                message: "The lunch is out the door. Literally. You lose time.".into(),
+                chance: 7,
+                time: 9,
+                ..Default::default()
+            },
+            events {
+                name: "FoodPoisoning".into(),
+                message: "The food you ate gave you food poisoning. You lose health and time.".into(),
+                chance: 1,
+                time: 2,
+                health: 5,
+                ..Default::default()
+            },
+            events {
+                name: "GreatLunch".into(),
+                message: "The line was not too long. Your hunger is satisfied".into(),
+                hunger: 8,
+                ..Default::default()
+            },
+        ];
+        
+        let randfoodOptions = vec![eventsVec[2], eventsVec[3], eventsVec[4]];
+    
+        eventsVec[1].addEvents(randfoodOptions);
+        
+    
         return eventsVec;
     }
+    
 }

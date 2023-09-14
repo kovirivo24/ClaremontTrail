ae511a3f3e3e64e6cab9546c32706c4205e67aa9
Caleb <Calebtheeperson@gmail.com>
2023-09-13 21:29:11 -0700

    Changed u8 -> i8 for negatives

--- src/player.rs
+++ src/player.rs
@@ -10,9 +10,9 @@ pub mod player {
     #[derive(Clone)]
     pub struct Player {
         pub name: String,
-        pub hunger: u8, // Biggest Number in a u8 is 255
-        pub time: u8,   // Maybe make this a float , look at later -agreed Kovit
-        pub health: u8,
+        pub hunger: i8, // Biggest Number in a u8 is 255
+        pub time: i8,   // Maybe make this a float , look at later -agreed Kovit
+        pub health: i8,
         pub item: Option<crate::item>,
     }
 
@@ -33,13 +33,13 @@ pub mod player {
         }
 
         //Idk what to fuckin call this variable tbh
-        pub fn updateHealth(&mut self, incrementer: u8) {
+        pub fn updateHealth(&mut self, incrementer: i8) {
             self.health -= incrementer;
         }
-        pub fn updateHunger(&mut self, incrementer: u8) {
+        pub fn updateHunger(&mut self, incrementer: i8) {
             self.hunger -= incrementer;
         }
-        pub fn updateTime(&mut self, incrementer: u8) {
+        pub fn updateTime(&mut self, incrementer: i8) {
             self.time -= incrementer;
         }
     }
